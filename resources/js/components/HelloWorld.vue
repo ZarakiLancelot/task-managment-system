b40694348eaa648658fa6761397568f8f32bb764
ZarakiLancelot <kenpachi2783@gmail.com>
2024-03-31 19:10:36 -0600

    MOD: including VUE

---
+++ resources/js/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup>
+    export default {
+        name: 'HelloWorld'
+    }
+</script>
+
+<template>
+    <div>
+        <h1>Hello World</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
